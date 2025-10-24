a154b76ef54c0c887869e9532c813fe4797f7c2b
fiqriridho <fiqri6149@gmail.com>
2025-10-24 12:30:42 +0700

    Update: Codelab Modul 3 Line 6

--- Codelab/Modul_3/src/Codelab1/Main.java
+++ Codelab/Modul_3/src/Codelab1/Main.java
@@ -3,5 +3,6 @@ package Codelab1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Welcome to Modul 3 Codelab!");
     }
 }
